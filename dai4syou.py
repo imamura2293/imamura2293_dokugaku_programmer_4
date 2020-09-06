7cfb9ef88e27ab18d15bd7ef1738ad4d0e7e4406
pana2 <imamura2293@gmail.com>
2020-09-06 12:27:08 +0900

    dai4syouyiduki

--- dai4syou.py
+++ dai4syou.py
@@ -118,3 +118,86 @@ print(f(4)
 def add_it(x,y=10)):
     return x + y
 
+result = add_it(2))
+print(result)
+
+X = 1
+y = 2
+z = 3
+
+def f():
+
+    X = 1
+    y = 2
+    z = 3
+
+    print(x)
+    print(y)
+    print(z)
+
+f()
+
+if x > 100:
+    print("x is > 100")
+
+
+x = 100
+
+def f():
+    global x
+    x += 1
+    print(x)
+f()
+
+a = input("type a number:")
+b = input("type another:")
+a = int(a)
+b = int(b)
+print(a / b)
+
+a = input("type a number:")
+b = input("type another:")
+a = int(a)
+b = int(b)
+print(a / b)
+
+
+a = input("type a number:")
+b = input("type another:")
+a = int(a)
+b = int(b)
+try:
+    print(a / b)
+except ZerodivisionError:
+    print("b cannot be zero")
+
+
+try:
+    a = input("type a number:")
+    b = input("type another:")
+    a = int(a)
+    b = int(b)
+    try:
+        print(a / b)
+    except (ZerodivisionError,ValueError):
+        print("Invalid input")
+
+try:
+    10 / 0
+    c = "I will never get defined."
+except ZeroDivisionError:
+    print(c)
+
+def add(x, y):
+    """
+    Returns x + y.
+    :param x: int.
+    :param y: int.
+    :return: int sum of x and y.
+    """
+    return x + y
+
+x = 100
+print(x)
+
+print(100)
