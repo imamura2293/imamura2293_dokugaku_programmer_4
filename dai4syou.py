31cb459d3cd2800bb9ac2998b79a8a82a94d0bab
pana2 <imamura2293@gmail.com>
2020-09-06 09:07:21 +0900

    dai4syouyiduki

---
+++ dai4syou.py
@@ -0,0 +1,60 @@
+f(x) = x * 2
+
+result = f(2)
+
+
+def f(x):
+    return x * 2
+
+
+result = f(2)
+print(result)
+
+
+def f(x):
+    return x + 1
+
+
+z = f(4)
+
+if z == 5
+    print("z is 5")
+else:
+    print("z is not 5")
+
+
+def f():
+    result = f()
+    print(result)
+
+def f(x,y,z):
+    return x + y + z
+
+result = f(1,2,3)
+print(result)
+
+def f():
+    z = 1 + 1
+
+result = f()
+print(result)
+
+len("Monty")
+
+len("Python")
+
+str(100)
+
+int("1")
+
+float(100)
+
+
+int("110")
+int(20.54)
+float("16.4")
+float(99)
+
+int("Prince")
+
+age =
